67fa4de709c8f197ec0939b1a62a7f7da31e0f13
Daniele Crespi <d.crespi@magrisgroup.local>
2017-10-26 15:05:22 +0200

    Added debugging options; updated categories-tree view

--- src/app/categories-tree/categories-tree.component.ts
+++ src/app/categories-tree/categories-tree.component.ts
@@ -55,26 +55,29 @@ export class CategoriesTreeComponent implements OnInit {
     var titleRoot = {id: counter, name: this.getColumnLabelByKey(this.ROOTLEVEL), children: [], type: "title"};
     for (var key in this.data){   
         counter += 1;
-        var root = {id: counter, name: key, children: [], type: this.ROOTLEVEL};
+        var root = {id: counter, name: key, children: [], type: this.ROOTLEVEL, numberOfArticles:0};
         counter += 1;
         root.children.push({id: counter, name: this.getColumnLabelByKey(this.SECONDLEVEL), children: [], type: "title"});
         for (var secondKey in this.data[key]){
           counter += 1;
-          var second = {id:counter, name: secondKey, children: [], type: this.SECONDLEVEL};
+          var second = {id:counter, name: secondKey, children: [], type: this.SECONDLEVEL, numberOfArticles:0};
           counter += 1;
           second.children.push({id: counter, name: this.getColumnLabelByKey(this.THIRDLEVEL), children: [], type: "title"});
           for (var thirdKey in this.data[key][secondKey]){
             counter += 1;
-            var third = {id:counter, name: thirdKey,  children: [], type: this.THIRDLEVEL};
+            var third = {id:counter, name: thirdKey,  children: [], type: this.THIRDLEVEL, numberOfArticles:0};
             for (var articleKey in this.data[key][secondKey][thirdKey]){
               counter += 1;
               var child = {id:counter, name:this.data[key][secondKey][thirdKey][articleKey].ARCODART, article:this.data[key][secondKey][thirdKey][articleKey]}
               third.children.push(child);
-            }              
+              third.numberOfArticles = third.children.length;
+            }    
+            second.numberOfArticles += third.children.length;          
             second.children.push(third);
           }
+          root.numberOfArticles += second.numberOfArticles;
           root.children.push(second);
-        }
+        }        
         this.nodes.push(root);
     }
     this.nodes.unshift(titleRoot);    
